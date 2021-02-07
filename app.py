229ef04e218900160e7e6e00a2d707219ff49362
yarmout <yarmout@gmail.com>
2021-02-07 02:13:09 +0000

    Implemented HTML and CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 import spotify_api
 
@@ -7,7 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     song = spotify_api.Spotify()
-    return song.Name
+    return render_template(
+        'index.html',
+        song_name = song.Name,
+        song_artists = song.Artists,
+        len_artists = len(song.Artists),
+        song_image = song.Image,
+        song_preview = song.Preview
+        )
         
 app.run(
     port = int(os.getenv('PORT', 8080)),
