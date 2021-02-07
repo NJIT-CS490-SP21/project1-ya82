9f00f71524a15208961337bd53835bd1024dc7c4
yarmout <yarmout@gmail.com>
2021-02-07 18:53:02 +0000

    Code cleanup/standards

--- app.py
+++ app.py
@@ -3,20 +3,22 @@ import os
 import spotify_api
 
 app = Flask(__name__)
-    
+
+
 @app.route('/')
-def hello_world():
+def homepage():
     song = spotify_api.Spotify()
     return render_template(
         'index.html',
-        song_name = song.Name,
-        song_artists = song.Artists,
-        len_artists = len(song.Artists),
-        song_image = song.Image,
-        song_preview = song.Preview
-        )
-        
-app.run(
-    port = int(os.getenv('PORT', 8080)),
-    debug=True
+        song_name=song.Name,
+        song_artists=song.Artists,
+        len_artists=len(song.Artists),
+        song_image=song.Image,
+        song_preview=song.Preview
     )
+
+
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    debug=True
+)
--- spotify_api.py
+++ spotify_api.py
@@ -3,65 +3,65 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import random
 
-load_dotenv(find_dotenv())
 
-# Retrieves access token
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-        
-params = {
-    'grant_type': 'client_credentials',
-    'client_id': os.getenv('SPOTIFY_CLIENT_ID'),
-    'client_secret': os.getenv('SPOTIFY_CLIENT_SECRET')
-}
-        
-response = requests.post(AUTH_URL, params)
-data = response.json()
-access_token = data['access_token']
-       
+def fetch_access_token():
+    load_dotenv(find_dotenv())
+
+    # Retrieves access token
+    auth_url = 'https://accounts.spotify.com/api/token'
+
+    params = {
+        'grant_type': 'client_credentials',
+        'client_id': os.getenv('SPOTIFY_CLIENT_ID'),
+        'client_secret': os.getenv('SPOTIFY_CLIENT_SECRET')
+    }
+
+    response = requests.post(auth_url, params)
+    data = response.json()
+    return data['access_token']
+
+
+access_token = fetch_access_token()
+
 
 class Spotify:
     """The Spotify API allows access to data about the songs, artists, and albums available on Spotify"""
-    
-    
     def __init__(self):
         artist_ids = {
             'Caravan Palace': '37J1PlAkhRK7yrZUtqaUpQ',
             'Of Monsters and Men': '4dwdTW1Lfiq0cM8nBAqIIz',
             'SIAMES': '68NOjWuVYBRXzYwhel3jAl'
         }
-        
+
         artist = random.choice(list(artist_ids))
-        
-        BASE_URL = 'https://api.spotify.com/v1/artists/{id}/top-tracks'.format(
+
+        base_url = 'https://api.spotify.com/v1/artists/{id}/top-tracks'.format(
             id=artist_ids[artist]
-            )
-        
+        )
+
         header = {
             'Authorization': 'Bearer {token}'.format(token=access_token)
         }
 
-        
         params = {
             'market': 'US'
         }
-        
-        
+
         response = requests.get(
-            BASE_URL,
+            base_url,
             headers=header,
             params=params
-            )
-            
+        )
+
         data = response.json()
         tracks = data['tracks']
         song = random.choice(tracks)
-        
+
         song_artists = []
         for artist in song['artists']:
             song_artists.append(artist['name'])
-            
+
         self.Name = song['name']
         self.Artists = song_artists
         self.Image = song['album']['images'][0]['url']
         self.Preview = song['preview_url']
-        
